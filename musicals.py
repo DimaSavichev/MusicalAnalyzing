d0b6efde12c41e6efa933cc414af1b42815b32e9
DimaSavichev <kubamba2002@gmail.com>
2021-05-11 00:07:20 +0300

    splitted lyrics into passages of characters

--- musicals.py
+++ musicals.py
@@ -1,3 +1,4 @@
+import string
 class Musical:
     def __init__(self, name, url):
         self.name = name
@@ -10,9 +11,32 @@ class Song:
         self.name = name
         self.lyrics = lyrics
 
+def ispunct(ch):
+    return ch in string.punctuation
+
+
+def getName(s):
+    i = 0
+    while i < len(s) and (s[i].isupper() or s[i] == ' ' or s[i] == '.') and (not ispunct(s[i]) or s[i] == '.'):
+        i += 1
+    return s[:i].strip()
+
 
 def analyze(lyrics):
-    pass
+    characters = dict()
+    while lyrics.find("[") != -1:
+        header = lyrics[lyrics.find("[") + 1: lyrics.find("]")]
+        name = getName(header)
+        lyrics = lyrics[lyrics.find("]") + 1:].strip()
+        paragraph = lyrics[: lyrics.find("[")].strip()
+        if paragraph != '':
+            if name not in characters.keys():
+                characters[name] = []
+            characters[name].append(paragraph)
 
+        lyrics = lyrics[lyrics.find("["):].strip()
+
+    for key, value in characters.items():
+        print(key + ": ", value)
 
 musicals = [Musical("Heathers", "https://genius.com/artists/Heathers-the-musical-ensemble")]
